d9458ba94d3f3a4a4e9daf74927ba95e67104078
PROMO\cegoncalves <cristian.goncalves@pst.asseco.com>
2021-04-20 17:51:26 +0100

    Fix problem related with uncaught exceptions

--- src/main/groovy/io/github/asseco/pst/infrastructure/exceptions/UncaughtExceptionsManager.groovy
+++ src/main/groovy/io/github/asseco/pst/infrastructure/exceptions/UncaughtExceptionsManager.groovy
@@ -1,10 +1,13 @@
 package io.github.asseco.pst.infrastructure.exceptions
 
 import io.github.asseco.pst.infrastructure.crawlers.Worker
+import io.github.asseco.pst.infrastructure.logs.LoggerFactory
+import org.slf4j.Logger
 
 @Singleton
 class UncaughtExceptionsManager {
 
+    private static final Logger logger = LoggerFactory.getLogger(UncaughtExceptionsManager.class)
     List<EThreadUncaughtExceptionHandler> uncaughtExceptions = []
 
     synchronized EThreadUncaughtExceptionHandler factory(Worker aObserver) {
@@ -15,14 +18,23 @@ class UncaughtExceptionsManager {
         return uncaughtExceptionHandler
     }
 
+    void reset() {
+        uncaughtExceptions = []
+    }
+
     /**
      * If a uncaught exception is found main thread is interrupted by throwing the first identified uncaught exception
      */
     void checkUncaughtExceptions() {
 
         uncaughtExceptions.each {
-            if(it.hasUncaughtExceptions)
+            if(it.hasUncaughtExceptions) {
+                logger.error("The following exception was thrown during the dependencies calculation: "
+                        + "Message: " + it.threadTrowable.getMessage()
+                            + "Cause: " + it.threadTrowable.getCause())
+
                 throw it.threadTrowable
+            }
         }
     }
 }
